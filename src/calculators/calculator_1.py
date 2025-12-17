6118f959eaeed0139fbc3c30abacb62cd9a92510
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-12-17 18:28:50 -0300

    criando segundo processo da calc 1

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -4,11 +4,13 @@ class Calculator1:
     def calculate(self, request: Request) -> dict:
         body = request.json
         input_data = self.__validate_body(body)
-
         splited_number = input_data / 3
         first_process = self.__first_process(splited_number)
-        
-
+        second_process = self.second_process(splited_number)
+        calc_result = first_process + second_process + splited_number
+        response = self.__format_response(calc_result)
+        return response
+    
     def __validate_body(self, body: dict) -> float:
         if "number" not in body:
             raise ValueError("Missing 'number' in request body")
@@ -18,6 +20,13 @@ class Calculator1:
         return float(input_data)
     
     def __first_process(self, number: float) -> float:
-        first_part = (number / 4) + 7
-        second_part = (first_part ** 2) * 0.257
-        return second_part
+        return (((number / 4) + 7) ** 2) * 0.257
+    
+    def second_process(self, number: float) -> float:
+        return ((number ** 2.121) / 5) + 1
+    
+    def __format_response(self, result: float) -> dict:
+        return {"data": {
+            "calculator": 1,
+            "result": round(result, 2)
+        }}
